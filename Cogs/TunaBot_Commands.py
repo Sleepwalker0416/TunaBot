f58e7713a1b2096b32c119fda434651f0b6fab15
Yuki <kingbavuong2002@gmail.com>
2021-09-26 18:57:01 +0700

    Push 3

--- Cogs/TunaBot_Commands.py
+++ Cogs/TunaBot_Commands.py
@@ -1,50 +1,35 @@
-#Tuna Bot
+#Tuna Bot Commands
 import discord
-from discord.ext import commands, tasks
-from itertools import cycle
+from discord.ext import commands
 from discord.ext.commands.errors import MissingRequiredArgument
 
-client = commands.Bot(command_prefix = 't')
-status = cycle(['Bullying everyone!', 'Taste the salt!'])
+class Command(commands.Cog):
+    
+    def __init__(self, client):
+        self.client = client
 
-#Ready check
-@client.event
-async def on_ready():
-    change_status.start()
-    print('Bot is ready!')
+    #Command
+    #Reaction command
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'Ping: {round(self.client.latency * 1000)} ms')
 
-#Tasks
-@tasks.loop(seconds=5)
-async def change_status():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game(next(status)))
+    @commands.command()
+    async def Tuna(self, ctx):
+        await ctx.send('Cuncec gì UwU')
 
-#Errors
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send('Invalid command!')
+    #Moderation command
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount: int):
+        await ctx.channel.purge(limit=amount)
+        await ctx.send(str(amount) + ' cleared messages')
 
-#Command
-#Reaction command
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'Ping: {round(client.latency * 1000)} ms')
+    #Clear error
+    @clear.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('Invalid argument!')
 
-@client.command()
-async def Tuna(ctx):
-    await ctx.send('Cuncec gì UwU')
-
-#Moderation command
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def clear(ctx, amount: int):
-    await ctx.channel.purge(limit=amount)
-    await ctx.send(str(amount) + ' cleared messages')
-
-#Clear error
-@clear.error
-async def clear_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Invalid argument!')
-
-client.run('')
+def setup(client):
+    client.add_cog(Command(client))
--- TunaBot_Main.py
+++ TunaBot_Main.py
@@ -1,15 +1,25 @@
-#Tuna Bot
+#Tuna Bot Main
 import discord
 import os
-from discord.ext import commands
+from itertools import cycle
+from discord.ext import commands, tasks
 from discord.ext.commands.errors import MissingRequiredArgument
 
 client = commands.Bot(command_prefix = 't')
 
+#Status cycle   
+status = cycle(['Bullying everyone!', 'Taste the salt!'])
+
 #Ready check
 @client.event
 async def on_ready():
+    change_status.start()
     print('Bot is ready!')
+
+#Tasks
+@tasks.loop(seconds=5)
+async def change_status():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game(next(status)))
 
 @client.command()
 async def load(ctx, extension):
